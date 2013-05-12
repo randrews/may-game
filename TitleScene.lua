bb0c985d1c2ee6e3d06cbbdf60e180ef2b7d1a53
Ross Andrews <ross.andrews@gmail.com>
2013-05-12 00:29:15 -0500

    Logo drops in from the top

--- TitleScene.lua
+++ TitleScene.lua
@@ -3,150 +3,16 @@ local List = sonnet.List
 local Point = sonnet.Point
 
 function TitleScene:initialize()
-    self.boxes = List()
-    
-    self.boxes:push{
-        Point(40, 20),
-        Point(60, 20),
-        Point(30, 80),
-        Point(10, 80),
-    }
+    self.logo = love.graphics.newImage('logo.png')
+    self.original_polygons = self.logo_points()
 
-    self.boxes:push{
-        Point(10, 80),
-        Point(70, 80),
-        Point(60, 100),
-        Point(0, 100),
-    }
+    local w, h = love.graphics.getMode()
+    local lw, lh = self.logo:getWidth(), self.logo:getHeight()
 
-    self.boxes:push{
-        Point(70, 100),
-        Point(110, 20),
-        Point(130, 20),
-        Point(90, 100),
-    }
-
-    self.boxes:push{
-        Point(110, 20),
-        Point(130, 20),
-        Point(170, 100),
-        Point(150, 100),
-    }
-
-    self.boxes:push{
-        Point(100, 80),
-        Point(140, 80),
-        Point(140, 60),
-        Point(100, 60),
-    }
-
-    self.boxes:push{
-        Point(45, 10),
-        Point(240, 10),
-        Point(240, 0),
-        Point(50, 0),
-    }
-
-    self.boxes:push{
-        Point(230, 0),
-        Point(250, 0),
-        Point(190, 120),
-        Point(170, 120),
-    }
-
-    self.boxes:push{
-        Point(190, 110),
-        Point(415, 110),
-        Point(410, 120),
-        Point(190, 120),
-    }
-
-    self.boxes:push{
-        Point(220, 100),
-        Point(260, 20),
-        Point(280, 20),
-        Point(240, 100),
-    }
-
-    self.boxes:push{
-        Point(220, 100),
-        Point(230, 80),
-        Point(290, 80),
-        Point(280, 100),
-    }
-
-    self.boxes:push{
-        Point(260, 20),
-        Point(320, 20),
-        Point(310, 40),
-        Point(260, 40),
-    }
-
-    self.boxes:push{
-        Point(255, 70),
-        Point(265, 50),
-        Point(285, 50),
-        Point(275, 70),
-    }
-
-    self.boxes:push{
-        Point(290, 100),
-        Point(330, 20),
-        Point(350, 20),
-        Point(310, 100),
-    }
-
-    self.boxes:push{
-        Point(330, 20),
-        Point(390, 20),
-        Point(380, 40),
-        Point(320, 40),
-    }
-
-    self.boxes:push{
-        Point(315, 50),
-        Point(375, 50),
-        Point(365, 70),
-        Point(305, 70),
-    }
-
-    self.boxes:push{
-        Point(360, 40),
-        Point(380, 40),
-        Point(375, 50),
-        Point(355, 50),
-    }
-
-    self.boxes:push{
-        Point(335, 70),
-        Point(355, 70),
-        Point(355, 100),
-        Point(335, 100),
-    }
-
-    self.boxes:push{
-        Point(360, 100),
-        Point(370, 80),
-        Point(430, 80),
-        Point(420, 100),
-    }
-
-    self.boxes:push{
-        Point(410, 80),
-        Point(390, 40),
-        Point(410, 40),
-        Point(430, 80),
-    }
-
-    self.boxes:push{
-        Point(390, 40),
-        Point(400, 20),
-        Point(460, 20),
-        Point(450, 40),
-    }
+    self.logo_x = w/2-lw/2
+    self.logo_y = sonnet.Tween(0, h/3-lh/2, 5)
 
     self.player = {loc=Point(400, 300), angle=-math.pi/2}
-    self.logo = love.graphics.newImage('logo.png')
 end
 
 function TitleScene:on_install()
@@ -177,7 +43,7 @@ function TitleScene:draw()
     local g = love.graphics
 
     g.setColor(255, 255, 255, 255)
-    g.draw(self.logo, 0, 0)
+    g.draw(self.logo, self.logo_x, self.logo_y.value)
 
     g.setColor(140, 180, 160)
     g.circle('fill',
@@ -188,7 +54,13 @@ function TitleScene:draw()
     local ey = self.player.loc.y + math.sin(self.player.angle)*32
     g.line(self.player.loc.x, self.player.loc.y, ex, ey)
 
-    local i = raycast(self.player.loc, self.player.angle, self.boxes)
+
+    local polygons = self:translate_shapes(self.original_polygons,
+                                           Point(self.logo_x, self.logo_y.value))
+
+    local i = self:raycast(self.player.loc,
+                           self.player.angle,
+                           polygons)
 
     g.setColor(0, 255, 0)
     for _, shape in i:each() do
@@ -204,7 +76,7 @@ end
 
 ----------------------------------------
 
-function raycast(pt, angle, shapes)
+function TitleScene:raycast(pt, angle, shapes)
     local rvec = Point(math.cos(angle), math.sin(angle))
 
     local intersections = List()
@@ -216,4 +88,160 @@ function raycast(pt, angle, shapes)
     end
 
     return intersections
+end
+
+function TitleScene:translate_shapes(shapes, delta)
+    return shapes:map(function(shape)
+                          local new_shape = {}
+                          for _, p in ipairs(shape) do
+                              table.insert(new_shape, p+delta)
+                          end
+                          return new_shape
+                      end)
+end
+
+function TitleScene:logo_points()
+    local polygons = List()
+
+    polygons:push{
+        Point(40, 20),
+        Point(60, 20),
+        Point(30, 80),
+        Point(10, 80),
+    }
+
+    polygons:push{
+        Point(10, 80),
+        Point(70, 80),
+        Point(60, 100),
+        Point(0, 100),
+    }
+
+    polygons:push{
+        Point(70, 100),
+        Point(110, 20),
+        Point(130, 20),
+        Point(90, 100),
+    }
+
+    polygons:push{
+        Point(110, 20),
+        Point(130, 20),
+        Point(170, 100),
+        Point(150, 100),
+    }
+
+    polygons:push{
+        Point(100, 80),
+        Point(140, 80),
+        Point(140, 60),
+        Point(100, 60),
+    }
+
+    polygons:push{
+        Point(45, 10),
+        Point(240, 10),
+        Point(240, 0),
+        Point(50, 0),
+    }
+
+    polygons:push{
+        Point(230, 0),
+        Point(250, 0),
+        Point(190, 120),
+        Point(170, 120),
+    }
+
+    polygons:push{
+        Point(190, 110),
+        Point(415, 110),
+        Point(410, 120),
+        Point(190, 120),
+    }
+
+    polygons:push{
+        Point(220, 100),
+        Point(260, 20),
+        Point(280, 20),
+        Point(240, 100),
+    }
+
+    polygons:push{
+        Point(220, 100),
+        Point(230, 80),
+        Point(290, 80),
+        Point(280, 100),
+    }
+
+    polygons:push{
+        Point(260, 20),
+        Point(320, 20),
+        Point(310, 40),
+        Point(260, 40),
+    }
+
+    polygons:push{
+        Point(255, 70),
+        Point(265, 50),
+        Point(285, 50),
+        Point(275, 70),
+    }
+
+    polygons:push{
+        Point(290, 100),
+        Point(330, 20),
+        Point(350, 20),
+        Point(310, 100),
+    }
+
+    polygons:push{
+        Point(330, 20),
+        Point(390, 20),
+        Point(380, 40),
+        Point(320, 40),
+    }
+
+    polygons:push{
+        Point(315, 50),
+        Point(375, 50),
+        Point(365, 70),
+        Point(305, 70),
+    }
+
+    polygons:push{
+        Point(360, 40),
+        Point(380, 40),
+        Point(375, 50),
+        Point(355, 50),
+    }
+
+    polygons:push{
+        Point(335, 70),
+        Point(355, 70),
+        Point(355, 100),
+        Point(335, 100),
+    }
+
+    polygons:push{
+        Point(360, 100),
+        Point(370, 80),
+        Point(430, 80),
+        Point(420, 100),
+    }
+
+    polygons:push{
+        Point(410, 80),
+        Point(390, 40),
+        Point(410, 40),
+        Point(430, 80),
+    }
+
+    polygons:push{
+        Point(390, 40),
+        Point(400, 20),
+        Point(460, 20),
+        Point(450, 40),
+    }
+
+    return polygons
 end
